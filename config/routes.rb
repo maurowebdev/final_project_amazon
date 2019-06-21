a65f9227d4e2c36d73fe35a9c080a9a99da8ce1d
Mauricio <cmbarrososorio@gmail.com>
2019-06-20 19:28:57 -0500

    Second commit, added partial headers and footers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'landing#index'
+  get 'landing/index'
+  get 'landing/about'
+#  get 'sobre_mi', to: 'landing#about'
+  get 'landing/contact'
+
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
